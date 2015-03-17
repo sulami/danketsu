3e37e77917c07c41df1b1009e98a0e6bf1d191ac
Robin Schroer <sulami@peerwire.org>
2015-03-17 15:06:43 +0100

    basic setup of a binary to run

--- danketsu.go
+++ danketsu.go
@@ -1,2 +1,15 @@
-package danketsu
+package main
+
+import (
+	"flag"
+	"strconv"
+	"net/http"
+)
+
+func main() {
+	port := flag.Int("port", 8080, "Port to listen on")
+	flag.Parse()
+
+	http.ListenAndServe(":" + strconv.Itoa(*port), nil)
+}
 
--- danketsu_test.go
+++ danketsu_test.go
@@ -1,4 +1,4 @@
-package danketsu
+package main
 
 import (
 	"testing"
